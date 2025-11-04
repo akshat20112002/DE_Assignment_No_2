cbc85dce7f1ff14e4d4ab5e6e70f0751e0afb693
akshat20112002 <akshat20112002@gmail.com>
2025-11-04 16:43:13 +0530

    Exercise-5 Commits and Fixes

--- Exercises/Exercise-5/db_setup/check_env.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # ensures .env variables are loaded in all terminals
-
-print("Loaded DB_USER:", os.getenv("DB_USER"))
-print("Loaded DB_PASSWORD:", os.getenv("DB_PASSWORD"))
